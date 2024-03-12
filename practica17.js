362140c33cceedd0ba794824ad509c691bb93e7b
Alexisrdrgz <rodriguez.alexis0404@gmail.com>
2024-03-11 19:20:03 -0700

    practica17

---
+++ practica17.js
@@ -0,0 +1,5 @@
+var a = 12;
+var b = 14;
+
+procesos = "La suma de " + a + " y " + b + " es: " + (a + b) + "\n" + "La resta de " + a + " y " + b + " es: " + (a - b) + "\n" + "La multiplicación de " + a + " y " + b + " es: " + (a * b) + "\n" + "La división de " + a + " y " + b + " es: " + (a / b) + "\n";
+alert(procesos)
